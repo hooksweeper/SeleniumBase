06bd4f182d3cafb53b399dfab4f31055289acab0
hooksweeper <96995128+hooksweeper@users.noreply.github.com>
2025-05-05 19:14:16 -0400

    Update test_doxbin.py 2
    uc_gui_click_captcha

--- examples/test_doxbin.py
+++ examples/test_doxbin.py
@@ -6,6 +6,6 @@ class MyTestClass(BaseCase):
     def test_doxbin(self):
         self.open("https://doxbin.com/")
         self.sleep(2)
-        self.uc_gui_handle_captcha()
+        self.uc_gui_click_captcha()
         self.sleep(2)
         self.assert_text("Doxbin")
