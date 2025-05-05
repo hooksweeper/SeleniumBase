b60631cfd8de30a2cab8dc11699666cf7f4ff70a
hooksweeper <96995128+hooksweeper@users.noreply.github.com>
2025-05-05 19:25:21 -0400

    Update test_doxbin.py 3

--- examples/test_doxbin.py
+++ examples/test_doxbin.py
@@ -4,8 +4,10 @@ BaseCase.main(__name__, __file__)
 
 class MyTestClass(BaseCase):
     def test_doxbin(self):
+        if not self.undetectable:
+            self.get_new_driver(undetectable=True)
         self.open("https://doxbin.com/")
         self.sleep(2)
         self.uc_gui_click_captcha()
         self.sleep(2)
-        self.assert_text("Doxbin")
+        self.assert_text("Pinned Pastes")
