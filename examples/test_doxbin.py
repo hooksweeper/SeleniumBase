9375ca3d0ee74e67e4f8a7d5d30c4819215a5000
hooksweeper <96995128+hooksweeper@users.noreply.github.com>
2025-05-05 18:53:25 -0400

    Create test_doxbin.py

---
+++ examples/test_doxbin.py
@@ -0,0 +1,11 @@
+from seleniumbase import BaseCase
+BaseCase.main(__name__, __file__)
+
+
+class MyTestClass(BaseCase):
+    def test_doxbin(self):
+        self.open("https://doxbin.com/")
+        self.sleeep(2)
+        self.uc_gui_handle_captcha()
+        self.sleep(2)
+        self.assert_text("Doxbin")
